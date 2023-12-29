9cdd048516d7346091e8a82e29ea67634f676eaa
tarumt <tarumt@B21>
2023-12-29 14:45:09 +0800

    Add project files.

---
+++ Prac9 (Materials)/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Prac9__Materials_
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs
+            Application.Lock();
+            Application["ErrorMsg"] += Server.GetLastError().Message;
+            Application.UnLock();
+        }
+
+    }
+}
